33ff94ed3084e68d7713c90c2a836dbb546a6c9b
Shiyen Huang <qaswedfr0308@gmail.com>
2024-05-28 23:47:29 +0800

    src/main/java/com/shiyen/groupingArt.sql

--- src/main/java/com/shiyen/groupingArt.sql
+++ src/main/java/com/shiyen/groupingArt.sql
@@ -91,10 +91,10 @@ CREATE TABLE report (
 	CONSTRAINT report_primary_key PRIMARY KEY (report_id)
 )AUTO_INCREMENT = 1;
 
-INSERT INTO report (report_issue,message_timestamp,report_result,report_reply, report_art_id,report_user_id)  
+INSERT INTO report (report_issue,report_timestamp,report_result,report_reply, report_art_id,report_user_id)  
 	VALUES ("不雅字眼","2024-05-21 14:07:57 ",TRUE,"檢舉已成功",4,1);
-INSERT INTO report (report_issue,message_timestamp,report_result,report_reply, report_art_id,report_user_id)  
-	VALUES ("辱罵","2024-05-44 14:07:57 ",FALSE,"檢舉已駁回",3,5);
-INSERT INTO report (report_issue,message_timestamp,report_result,report_reply, report_art_id,report_user_id)  
+INSERT INTO report (report_issue,report_timestamp,report_result,report_reply, report_art_id,report_user_id)  
+	VALUES ("辱罵","2024-05-10 14:07:57 ",FALSE,"檢舉已駁回",3,5);
+INSERT INTO report (report_issue,report_timestamp,report_result,report_reply, report_art_id,report_user_id)  
 	VALUES ("歧視","2024-05-22 14:03:57 ",TRUE,"檢舉已成功",2,3);
 
