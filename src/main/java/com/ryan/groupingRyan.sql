8914c2bed5f9893245df98fc8b47effef442dafa
Ryan <h29387465@gmail.com>
2024-05-29 15:19:36 +0800

    MYSQL update again

--- src/main/java/com/ryan/groupingRyan.sql
+++ src/main/java/com/ryan/groupingRyan.sql
@@ -10,18 +10,18 @@ CREATE TABLE subs (
     subs_art_id INT,
     subs_status boolean,
     CONSTRAINT fk_user FOREIGN KEY (subs_user_id) REFERENCES user(user_id),
-    CONSTRAINT fk_game FOREIGN KEY (subs_game_id) REFERENCES game(game_id)-- ,
-    -- CONSTRAINT fk_art FOREIGN KEY (subs_art_id) REFERENCES art(art_id) 
+    CONSTRAINT fk_game FOREIGN KEY (subs_game_id) REFERENCES game(game_id),
+    CONSTRAINT fk_art FOREIGN KEY (subs_art_id) REFERENCES art(art_id) 
 );
 
 INSERT INTO subs (subs_user_id, subs_game_id, subs_art_id, subs_status) VALUES
-(1, 1, 1, TRUE),
-(2, 2, 2, FALSE),
+(1, 1, null, TRUE),
+(2, null, 2, FALSE),
 (1, 3, NULL, TRUE),
 (3, NULL, 4, FALSE),
-(2, 5, 5, TRUE),
+(2, null, 5, TRUE),
 (4, 6, NULL, FALSE),
-(3, 7, 2, TRUE),
-(1, NULL, 3, FALSE),
+(3, 7, null, TRUE),
+(5, NULL, 3, FALSE),
 (2, 9, NULL, TRUE),
 (4, 10, NULL, FALSE);
